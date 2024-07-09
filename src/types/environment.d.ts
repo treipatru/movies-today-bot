c86c22b26029fe38eabd1b5621b286e68dbb99a7
treipatru <andrei@planet34.org>
2024-07-09 10:21:40 +0200

    ci: force ts compiler to compile all sources
    Running the jobs will not imply importing them directly therefore the
    compiler was ignoring them.

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -1,9 +1,9 @@
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
-			MOVIEDB_API_KEY: string;
 			MASTODON_ACCESS_TOKEN: string;
 			MASTODON_BASE_URL: string;
+			MOVIEDB_API_KEY: string;
 		}
 	}
 }
