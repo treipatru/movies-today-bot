2deeeb01f9028494653a31ea9e6655222ed34e9f
treipatru <andrei@planet34.org>
2024-07-09 14:54:01 +0200

    feat: add method to construct a movie description

---
+++ src/actions/movie/get-movie-info.ts
@@ -0,0 +1,24 @@
+import { format } from 'date-fns';
+import { type Movie } from '../../types/moviedb.js';
+
+export function getMovieInfo({
+	id,
+	overview,
+	release_date,
+	title,
+}: Movie) {
+	/**
+	 * Format the date to a more pleasant format.
+	 * @see https://date-fns.org/v3.6.0/docs/format
+	 */
+	const formattedDate = format(new Date(release_date), 'PPP');
+
+	const movieUrl = `https://www.themoviedb.org/movie/${id}`;
+
+	return `\n
+		${title} {${formattedDate}}\n
+		${overview} \n
+		${movieUrl} \n
+		#movie #film #cinema
+	`;
+}
