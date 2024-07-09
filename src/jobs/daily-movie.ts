91dec7f62e78cbd601f8c1f6ea42d5939b894f1c
treipatru <andrei@planet34.org>
2024-07-09 11:32:39 +0200

    refactor: make job compatible with bree scheduler

--- src/jobs/daily-movie.ts
+++ src/jobs/daily-movie.ts
@@ -1,4 +1,5 @@
 import { differenceInHours } from 'date-fns';
+import process from 'node:process';
 import { mastoApi, mastodon } from '../api/mastodon/masto.js';
 import { getMovie } from '../scripts/get-movie.js';
 import { postMovie } from '../scripts/post-movie.js';
@@ -14,19 +15,17 @@ function hasPostedToday(posts: mastodon.v1.Status[]) {
 	return hoursDiff < 24;
 }
 
-export async function dailyMovie() {
-	const posts = await mastoApi
-		.v1
-		.timelines
-		.home
-		.list({ limit: 1 });
-	const hasPosted = hasPostedToday(posts);
+const posts = await mastoApi
+	.v1
+	.timelines
+	.home
+	.list({ limit: 1 });
+const hasPosted = hasPostedToday(posts);
 
-	if (hasPosted) {
-		console.log('Already posted today.');
-		return;
-	}
-
-	const movie = await getMovie();
-	await postMovie(movie);
+if (hasPosted) {
+	console.log('Already posted today.');
+	process.exit(0);
 }
+
+const movie = await getMovie();
+await postMovie(movie);
