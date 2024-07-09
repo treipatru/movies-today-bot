d9312e83e28a3f1e2966749d459e974fec18f745
treipatru <andrei@planet34.org>
2024-07-09 10:23:11 +0200

    feat: add util to get node dir path

---
+++ src/utils/get-dir-path.ts
@@ -0,0 +1,13 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+/**
+ * Get the directory name of a module.
+ * This is a workaround of `__dirname` not being available in ES modules.
+ *
+ * @param moduleUrl The URL of the module.
+ */
+export function getDirPath(moduleUrl: string) {
+	const filename = fileURLToPath(moduleUrl);
+	return path.dirname(filename);
+}
