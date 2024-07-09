f0f1ebd817fe39f0e54bd5023d5ad15b73386a87
treipatru <andrei@planet34.org>
2024-07-09 14:51:13 +0200

    feat: add method to check last post

---
+++ src/actions/post/has-posted-today.ts
@@ -0,0 +1,20 @@
+import { differenceInHours } from 'date-fns';
+import { Status } from '../../types/mastodon.js';
+
+/**
+ * Check if the last post was made today.
+ *
+ * @export
+ * @param {Status[]} posts
+ * @return {boolean} True if the last post was made today
+ */
+export function hasPostedToday(posts: Status[]): boolean {
+	if (posts.length === 0) {
+		return false;
+	}
+
+	const now = new Date();
+	const hoursDiff = differenceInHours(now, posts[0].createdAt);
+
+	return hoursDiff < 24;
+}
