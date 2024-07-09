93a05e79f063c48a35b6266b7c419bdc7334cb41
treipatru <andrei@planet34.org>
2024-07-09 11:32:26 +0200

    feat: add job scheduler

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-// TODO Set up scheduler
+import Bree from 'bree';
+import path from 'path';
+import { getDirPath } from './utils/get-dir-path.js';
+
+const __dirname = getDirPath(import.meta.url);
+
+const bree = new Bree({
+	root: path.resolve(__dirname, 'jobs'),
+	jobs: [
+		{
+			name: 'daily-movie',
+			timeout: '10s',
+			interval: '1d',
+		},
+	],
+});
+
+console.log('Starting Bree...');
+await bree.start();
