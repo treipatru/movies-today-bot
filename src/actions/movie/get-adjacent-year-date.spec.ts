994912bc941829117bcc439836a93de0a56c630a
treipatru <andrei@planet34.org>
2024-07-10 11:52:05 +0200

    fix: fix and refactor adjacent year date method
    Initial implementation was completely wrong as it was
    incrementing/decrementing the year instead of incrementing/decrementing
    the date AND setting the year.

--- src/actions/movie/get-adjacent-year-date.spec.ts
+++ src/actions/movie/get-adjacent-year-date.spec.ts
@@ -1,18 +1,38 @@
-import { getAdjacentYearDate } from '@/actions/movie/get-adjacent-year-date.js';
+import { getAdjacentDayInYear } from '@/actions/movie/get-adjacent-year-date.js';
 import { getFormattedDate } from '@/utils/get-formatted-date.js';
 
-test("should return the previous year's date", () => {
+test("should return yesterday's date of the given year", () => {
 	const today = new Date();
-	const expected = getFormattedDate(new Date(2021, today.getMonth(), today.getDate()));
+	const expected = getFormattedDate(
+		new Date(
+			2021,
+			today.getMonth(),
+			today.getDate() - 1,
+		),
+	);
 
-	const result = getAdjacentYearDate(2022, 'previous');
+	const result = getAdjacentDayInYear({
+		date: today,
+		direction: 'previous',
+		year: 2021,
+	});
 	expect(result).toEqual(expected);
 });
 
-test("should return the next year's date", () => {
+test("should return tomorrow's date of the given year", () => {
 	const today = new Date();
-	const expected = getFormattedDate(new Date(2023, today.getMonth(), today.getDate()));
+	const expected = getFormattedDate(
+		new Date(
+			2023,
+			today.getMonth(),
+			today.getDate() + 1,
+		),
+	);
 
-	const result = getAdjacentYearDate(2022, 'next');
+	const result = getAdjacentDayInYear({
+		date: today,
+		direction: 'next',
+		year: 2023,
+	});
 	expect(result).toEqual(expected);
 });
--- src/actions/movie/get-adjacent-year-date.ts
+++ src/actions/movie/get-adjacent-year-date.ts
@@ -1,25 +1,47 @@
 import { getFormattedDate } from '@/utils/get-formatted-date.js';
 
-type AdjacentYear = 'previous' | 'next';
+type Direction = 'previous' | 'next';
 
 /**
- * TODO: Take leap years into consideration.
+ * Using a given date, get the adjacent day in a specified year.
+ * Easily get Useful for getting yesterday's or tomorrow's date in a specific year.
  *
+ * TODO: Take leap years into consideration.
  * Individual months have the same length in days every year.
  * Except for February, obviously.
+ *
+ * @export
+ * @param {{
+ * 	date: Date;
+ * 	direction: Direction;
+ * 	year: number;
+ * }} {
+ * 	date,
+ * 	direction,
+ * 	year,
+ * }
+ * @return {*} Formatted date string
  */
-
-export function getAdjacentYearDate(year: number, direction: AdjacentYear) {
-	const date = new Date();
+export function getAdjacentDayInYear({
+	date,
+	direction,
+	year,
+}: {
+	date: Date;
+	direction: Direction;
+	year: number;
+}): string {
+	const newDate = new Date(date);
+	newDate.setFullYear(year);
 
 	switch (direction) {
 		case 'previous':
-			date.setFullYear(year - 1);
+			newDate.setDate(date.getDate() - 1);
 			break;
 		case 'next':
-			date.setFullYear(year + 1);
+			newDate.setDate(date.getDate() + 1);
 			break;
 	}
 
-	return getFormattedDate(date);
+	return getFormattedDate(newDate);
 }
