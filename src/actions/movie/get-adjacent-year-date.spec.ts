173dfb17fac57bcc413c9ebb94afaf35e4ba3a1a
treipatru <andrei@planet34.org>
2024-07-10 12:03:44 +0200

    test: improve adjacent year tests

--- src/actions/movie/get-adjacent-year-date.spec.ts
+++ src/actions/movie/get-adjacent-year-date.spec.ts
@@ -36,3 +36,41 @@ test("should return tomorrow's date of the given year", () => {
 	});
 	expect(result).toEqual(expected);
 });
+
+test('should return expected date when previous day is in another month', () => {
+	const targetDate = new Date('2021-04-01');
+	const expected = getFormattedDate(new Date('1999-03-31'));
+
+	const result = getAdjacentDayInYear({
+		date: targetDate,
+		direction: 'previous',
+		year: 1999,
+	});
+	expect(result).toEqual(expected);
+});
+
+test('should return expected date when next day is in another month', () => {
+	const targetDate = new Date('2021-03-31');
+	const expected = getFormattedDate(new Date('1999-04-01'));
+
+	const result = getAdjacentDayInYear({
+		date: targetDate,
+		direction: 'next',
+		year: 1999,
+	});
+	expect(result).toEqual(expected);
+});
+
+test.fails('should NOT return expected date in leap year', () => {
+	// TODO: Fix leap year handling
+
+	const targetDate = new Date('2020-02-29');
+	const expected = getFormattedDate(new Date('2021-03-01'));
+
+	const result = getAdjacentDayInYear({
+		date: targetDate,
+		direction: 'next',
+		year: 2021,
+	});
+	expect(result).toEqual(expected);
+});
