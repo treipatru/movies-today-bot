fa7c96b56a94925df462f74b579de5edc44abc9f
treipatru <andrei@planet34.org>
2024-07-10 14:56:18 +0200

    refactor: improve daily movie job
    * Consolidated first implementation of separate scripts into proper job
      source
    * Use updated methods
    * Add comments

--- src/jobs/daily-movie.ts
+++ src/jobs/daily-movie.ts
@@ -1,23 +1,20 @@
-import { differenceInHours } from 'date-fns';
 import process from 'node:process';
-import { mastoApi, mastodon } from '../api/mastodon/masto.js';
-import { getMovie } from '../scripts/get-movie.js';
-import { postMovie } from '../scripts/post-movie.js';
+import { getAdjacentDayInYear } from '../actions/movie/get-adjacent-year-date.js';
+import { getBestMovieByDate } from '../actions/movie/get-best-movie.js';
+import { getMovieInfo } from '../actions/movie/get-movie-info.js';
+import { getYearsRange } from '../actions/movie/get-years-range.js';
+import { hasPostedToday } from '../actions/post/has-posted-today.js';
+import {
+	createMedia,
+	createStatus,
+	getTimeline,
+} from '../api/mastodon.js';
+import { getDiscoverMovie } from '../api/moviedb.js';
 
-function hasPostedToday(posts: mastodon.v1.Status[]) {
-	if (posts.length === 0) {
-		return false;
-	}
-
-	const now = new Date();
-	const hoursDiff = differenceInHours(now, posts[0].createdAt);
-
-	return hoursDiff < 24;
-}
-
-const posts = await mastoApi
-	.v1
-	.timelines
+/**
+ * Avoid posting multiple times in a day by checking the timeline.
+ */
+const posts = await getTimeline
 	.home
 	.list({ limit: 1 });
 const hasPosted = hasPostedToday(posts);
@@ -27,5 +24,76 @@ if (hasPosted) {
 	process.exit(0);
 }
 
-const movie = await getMovie();
-await postMovie(movie);
+/**
+ * Discover movies via the Movie Database API and
+ * get the best movie for today.
+ */
+
+const releaseYears = getYearsRange();
+const moviesFromThisDate = await Promise
+	.all(
+		releaseYears
+			.map(async releaseYear => {
+				/**
+				 * The Movie Database API does not have a method to request movies
+				 * released on a particular day so we have to specify a range.
+				 * If we're interested in movies released on a specific day, we need
+				 * to specify the day before and after.
+				 */
+				const dayBefore = getAdjacentDayInYear({
+					date: new Date(),
+					direction: 'previous',
+					year: releaseYear,
+				});
+				const dayAfter = getAdjacentDayInYear({
+					date: new Date(),
+					direction: 'next',
+					year: releaseYear,
+				});
+
+				/**
+				 * Get an array of movies released on today's month and day for the
+				 * current release year.
+				 */
+				const discoverResponse = await getDiscoverMovie({
+					'primary_release_date.gte': dayBefore,
+					'primary_release_date.lte': dayAfter,
+					'sort_by': 'popularity.desc',
+					'with_runtime.gte': '60',
+					'without_genres': '99', // Exclude documentaries
+				});
+
+				return discoverResponse.results;
+			}),
+	);
+
+const bestMovie = getBestMovieByDate(
+	moviesFromThisDate.flat(),
+	new Date(),
+);
+
+/**
+ * Create a status to post on Mastodon
+ */
+const status = getMovieInfo(bestMovie);
+
+/**
+ * Attach a movie poster to the status. The bestMovie will always have a poster.
+ */
+const remotePoster = await fetch(`https://image.tmdb.org/t/p/original/${bestMovie.poster_path}`);
+const statusAttachment = await createMedia({
+	description: `${bestMovie.title} movie poster`,
+	file: await remotePoster.blob(),
+});
+
+/**
+ * Post to Mastodon.
+ */
+await createStatus({
+	status,
+	mediaIds: statusAttachment ? [statusAttachment.id] : undefined,
+	visibility: 'public',
+});
+
+console.log('Posted movie: ', bestMovie.title);
+process.exit(0);
