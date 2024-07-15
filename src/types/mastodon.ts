ceb7ddee0ccdd50eb68022b189487f9fec0bbae2
treipatru <andrei@planet34.org>
2024-07-15 11:07:10 +0200

    refactor: export attachment params from mastodon types

--- src/types/mastodon.ts
+++ src/types/mastodon.ts
@@ -3,3 +3,5 @@ import { mastodon } from 'masto';
 export type Status = mastodon.v1.Status;
 
 export type MediaAttachment = mastodon.v1.MediaAttachment;
+
+export type CreateMediaAttachmentParams = mastodon.rest.v1.CreateMediaAttachmentParams;
